d907563fa8cdbf3f92e2487b195ab39910af15d8
Carlos Valencia Henao <rasfreed@gmail.com>
2023-12-03 21:46:18 -0500

    WIP - fixing build error

---
+++ src/app/admon/page.tsx
@@ -0,0 +1,13 @@
+type TPageProps = {
+  children: React.ReactNode;
+}
+
+const Admon = ({ children }: TPageProps) => {
+  return (
+    <>
+      {children}
+    </>
+  );
+}
+
+export default Admon
