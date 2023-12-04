d63a041d5e2f095c4810211084b28adcb1d4c554
Carlos Valencia Henao <rasfreed@gmail.com>
2023-12-03 20:46:29 -0500

    space was added to generate a re-deploy

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="w-full h-12 flex justify-center items-center bg-slate-900">
-      <h3 className="text-white">@Todos los derechos reservados</h3>
+      <h3 className="text-white">@Todos los derechos reservados </h3>
     </div>
   )
 }
